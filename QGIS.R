5fee4202174dd021a70edfb436f2663f65ee2650
Adib-fayad <adibfayad1@gmail.com>
2026-03-04 11:45:02 +0100

    GPKG

--- QGIS.R
+++ QGIS.R
@@ -1,40 +1,36 @@
-# --- 1. Chargement des librairies ---
 library(sf)    # Pour le vecteur (étangs, routes, BV)
 library(terra) # Pour le raster (LiDAR)
 library(dplyr) # Pour la manipulation des tables
 
-# --- 2. Chemins des fichiers (A adapter) ---
-path_etangs <- "SIG/Etg_complet.shp"
-path_routes <- "data/ain_routes.shp"
-path_lidar  <- "data/ain_lidar.tif"
-path_bv     <- "data/bassins_versants.shp"
-
+path_etangs <- "SIG/inpe.gpkg"
+path_routes <- "SIG/Route/ROUTE_NUMEROTEE_OU_NOMMEE.shp"
+path_lidar  <- "SIG/Dombes_2021_MNT_50cm_L93.cog.tif"
+path_bv     <- "SIG/Hydrologie/BV_test.shp"
+path_Fosses     <- "SIG/Hydrologie/Fosses_test.shp"
+path_OS     <- "SIG/OCCUPATION_SOL.gpkg"
 dir.create("GPKG_Sortie", showWarnings = FALSE)
 
-# --- 3. Chargement des couches sources ---
+# Chargement des couches sources
 # On charge tout une seule fois en mémoire pour gagner du temps
-etangs_ain <- st_read(path_etangs)
-routes_ain <- st_read(path_routes)
-lidar_ain  <- rast(path_lidar)
-tous_les_bv <- st_read(path_bv)
+etangs <- st_read(path_etangs)
+routes <- st_read(path_routes)
+lidar  <- rast(path_lidar)
+bv <- st_read(path_bv)
+fosses <- st_read(path_Fosses)
+OS <- st_read(path_OS)
 
-# --- 4. Traitement pour UN Bassin Versant (Test) ---
 # On prend le premier BV pour l'exemple
 bv_selection <- tous_les_bv[1, ] 
-nom_bv <- bv_selection$NOM_BV # Vérifie le nom de ta colonne Nom
+nom_bv <- bv_selection$layer 
 
-# Création du Buffer (ex: 500m)
-# Attention : Assure-toi d'être en Lambert-93 (unité = mètres)
-bv_buffer <- st_buffer(bv_selection, dist = 500)
+# Création du Buffer (ex: 250)
+bv_buffer <- st_buffer(bv_selection, dist = 250)
 
-# --- 5. Découpage et Mise en forme des tables ---
-
-# A. ETANGS
-etangs_final <- etangs_ain %>%
-  st_make_valid() %>% # Sécurité géométrie
+# ETANGS
+etangs_final <- etangs %>%
+  st_make_valid() %>%
   st_intersection(bv_buffer) %>%
-  # On nettoie et on crée les colonnes de saisie
-  select(ID_ORIGINE = 1) %>% # On garde juste l'ID d'origine
+  select(ID_ORIGINE = 1) %>% 
   mutate(
     NOM_ETANG  = NA_character_,  # Pour saisie texte
     TYPE_USAGE = "Inconnu",      # Liste déroulante future
@@ -43,25 +39,28 @@ etangs_final <- etangs_ain %>%
     COMMENT    = NA_character_   # Texte libre
   )
 
-# B. ROUTES
-routes_final <- routes_ain %>%
+# ROUTES
+routes_final <- routes %>%
   st_intersection(bv_buffer) %>%
-  select(TYPE_ROUTE = 1) # On garde le strict minimum
+  select(TYPE_ROUTE = 1) 
 
-# C. LIDAR (Découpe raster)
+#LIDAR (Découpe raster)
 # crop coupe le rectangle, mask coupe selon la forme exacte du buffer
-lidar_final <- crop(lidar_ain, bv_buffer, mask = TRUE)
+lidar_final <- crop(lidar, bv_buffer)
 
-# --- 6. Exportation dans le GeoPackage ---
-gpkg_path <- paste0("GPKG_Sortie/Etude_", nom_bv, ".gpkg")
+# Définir le nom du fichier GeoPackage de sortie
+nom_fichier_gpkg <- paste0("GPKG_Sortie/BV_", nom_bv, "_Complet.gpkg")
 
-# Première couche : on écrase le fichier s'il existe (delete_dsn)
-st_write(etangs_final, gpkg_path, layer = "etangs", delete_dsn = TRUE)
+# auvegarde des VECTEURS
+st_write(etangs_final, nom_fichier_gpkg, layer = "Etangs", append = FALSE) 
+st_write(routes_final, nom_fichier_gpkg, layer = "Routes", append = TRUE)
 
-# Couches suivantes : on ajoute au même fichier (append)
-st_write(routes_final, gpkg_path, layer = "routes", append = TRUE)
-
-# Pour le LiDAR (Raster)
-writeRaster(lidar_final, gpkg_path, name = "lidar", overwrite = TRUE, gdal="APPEND_SUBDATASET=YES")
-
-print(paste("GeoPackage généré :", gpkg_path))
+# Sauvegarde du RASTER DANS LE MÊME FICHIER
+# On utilise des arguments spéciaux (gdal) pour forcer l'ajout sans écraser le reste
+writeRaster(
+  lidar_final, 
+  filename = nom_fichier_gpkg, 
+  filetype = "GPKG",
+  gdal = c("APPEND_SUBDATASET=YES", "RASTER_TABLE=MNT_50cm") 
+)
+print(paste("Génial, tout est dans le même GeoPackage pour le BV :", nom_bv))
