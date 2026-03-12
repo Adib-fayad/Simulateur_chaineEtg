67af2af5c72f467042e47818a84e02a44717ac42
Adib-fayad <adibfayad1@gmail.com>
2026-03-12 09:50:24 +0100

    final QGIS

--- QGIS.R
+++ QGIS.R
@@ -45,155 +45,154 @@ OS <- st_read(path_OS)
 
 #nrow(bv)
 for (i in which(bv$CODE %in% c(19,1))) {
-  # On prend le premier BV pour l'exemple
-  bv_selection <- bv[i, ] 
-  nom_bv <- bv_selection$CODE 
-  print(paste("--- Traitement du BV :", nom_bv, "(", i, "/", nrow(bv), ") ---"))
-  # Création du Buffer 
-  bv_buffer <- st_buffer(bv_selection, dist = 2000)
-  
-  # ETANGS
-  etangs_final <- etangs %>%
-    st_make_valid() %>%
-    st_intersection(bv_buffer) %>%
-    mutate(
-      CODE_ETANG = 0,                 
-      NOM        = stringr::str_extract(nom_p_eau, "(?<=\": \").*(?=\")"),
-      SURFACE_eau = superficie,
-      Exutoire_1 = NA_character_,
-      Exutoire_2 = NA_character_
-      
-    ) %>% 
-    select(nature, insee_com, NOM, SURFACE_eau, CODE_ETANG, Exutoire_1,Exutoire_2)
-  
-  
-  # ROUTES
-  routes_final <- routes %>%
-    st_intersection(bv_buffer) %>%
-    select(TYPE_ROUTE = 1) 
-  
-  #os
-  os_final <- OS %>%
-    st_intersection(bv_buffer) %>%
-    select(Classe) 
-  
-  
-  
-  #LIDAR (Découpe raster)
-  # crop coupe le rectangle, mask coupe selon la forme exacte du buffer
-  lidar_final <- crop(lidar, bv_buffer)
-  lidar_5m <- aggregate(lidar_final, fact = 10, fun = "mean")
-  
-  print(paste("Lancement de l'hydrologie GRASS pour le BV :", nom_bv))
-  
-  mnt_temp <- "GPKG_Sortie/temp_mnt.tif"
-  writeRaster(lidar_5m, mnt_temp, overwrite = TRUE)
-  
-  #Préparation des chemins de sortie pour les 4 fichiers 
-  acc_tif  <- paste0("GPKG_Sortie/temp_acc_", nom_bv, ".tif")
-  dir_tif  <- paste0("GPKG_Sortie/temp_dir_", nom_bv, ".tif")
-  bas_tif  <- paste0("GPKG_Sortie/temp_bas_", nom_bv, ".tif")
-  demi_tif <- paste0("GPKG_Sortie/temp_demi_", nom_bv, ".tif")
-  
-  seuil_pixels <- (as.numeric(bv_selection$SURFACE) * 10000) / 25
-  
-  
-  
-  qgis_run_algorithm(
-    "grass:r.watershed",
-    elevation    = mnt_temp,
-    threshold    = seuil_pixels,
-    accumulation = acc_tif,
-    drainage     = dir_tif,
-    basin        = bas_tif,
-    half_basin   = demi_tif,
-    #"-s"         = TRUE,
-    .quiet       = TRUE 
-  )
-  
-  
-  bas_poly <- as.polygons(rast(bas_tif)) %>% 
-    st_as_sf() %>% 
-    st_make_valid()
-  
-  demi_poly <- as.polygons(rast(demi_tif)) %>% 
-    st_as_sf() %>% 
-    st_make_valid()
-  
-  
-  
-  # Définir le nom du fichier GeoPackage de sortie
-  nom_fichier_gpkg <- paste0("GPKG_Sortie/BV_", nom_bv, "_Complet.gpkg")
-  if (file.exists(nom_fichier_gpkg)) file.remove(nom_fichier_gpkg)
-  
-  
-  
-  writeRaster(
-    lidar_final, 
-    filename = nom_fichier_gpkg, 
-    names = "MNT_50cm",
-    filetype = "GPKG",
-    overwrite = TRUE
-  )
-  
-  writeRaster(
-    lidar_5m, 
-    filename = nom_fichier_gpkg, 
-    names = "MNT_5m",
-    filetype = "GPKG",
-    gdal = c("APPEND_SUBDATASET=YES", "RASTER_TABLE=MNT_5m")
-  )
-  
-  writeRaster(rast(acc_tif),  filename = nom_fichier_gpkg, names = "Accumulation",       filetype = "GPKG",datatype = "FLT4S", gdal = c("APPEND_SUBDATASET=YES", "RASTER_TABLE=Accumulation"))
-  writeRaster(rast(dir_tif),  filename = nom_fichier_gpkg, names = "Direction_Drainage", filetype = "GPKG", gdal = c("APPEND_SUBDATASET=YES", "RASTER_TABLE=Direction_Drainage"))
-  
-  # auvegarde des VECTEURS
-  st_write(etangs_final, nom_fichier_gpkg, layer = "Etangs", append = TRUE, quiet = TRUE, layer_options = "GEOMETRY_NAME=geom") 
-  st_write(routes_final, nom_fichier_gpkg, layer = "Routes", append = TRUE, quiet = TRUE, layer_options = "GEOMETRY_NAME=geom")
-  st_write(os_final, nom_fichier_gpkg, layer = "OS", append = TRUE, quiet = TRUE, layer_options = "GEOMETRY_NAME=geom") 
-  st_write(bv_selection, nom_fichier_gpkg, layer = "bv_life", append = TRUE, quiet = TRUE, layer_options = "GEOMETRY_NAME=geom") 
-  st_write(bas_poly,  nom_fichier_gpkg, layer = "Bassins_Versant", append = TRUE, quiet = TRUE, layer_options = "GEOMETRY_NAME=geom")
-  st_write(demi_poly, nom_fichier_gpkg, layer = "Demi_Bassins", append = TRUE, quiet = TRUE, layer_options = "GEOMETRY_NAME=geom")
-  
- 
-  
-  
-  #style
-  con_dest <- dbConnect(SQLite(), nom_fichier_gpkg)
-  
-  #STYLE ÉTANGS
-  style_etangs <- style_data
-  style_etangs$f_table_name <- "Etangs"
-  style_etangs$f_geometry_column <- "geom"
-  style_etangs$useasdefault <- 1   
-  
-  #STYLE OS
-  style_os <- style_os_modele
-  style_os$f_table_name <- "OS"
-  style_os$f_geometry_column <- "geom"
-  style_os$useasdefault <- 1       
-  
-  #STYLE ACCUMULATION
-  style_acc <- style_acc_modele
-  style_acc$f_table_name <- "Accumulation"
-  style_acc$f_geometry_column <- ""  
-  style_acc$styleqml <- qml_texte
-  style_acc$stylename <- "Style Accumulation"
-  style_acc$useasdefault <- 1
-  
-  # On assemble tout
-  styles_complet <- rbind(style_etangs, style_os, style_acc)
-  if("id" %in% names(styles_complet)) styles_complet <- styles_complet %>% select(-id)
-  
-  # Écriture dans SQLite
-  dbWriteTable(con_dest, "layer_styles", styles_complet, overwrite = TRUE, row.names = FALSE)
-  dbExecute(con_dest, "CREATE INDEX IF NOT EXISTS msg_layer_styles_idx ON layer_styles (f_table_name, f_geometry_column);")
-  dbDisconnect(con_dest)
-  
-  # Nettoyage
-  fichiers_a_supprimer <- list.files(path = "GPKG_Sortie", pattern = "^temp_", full.names = TRUE)
-  file.remove(fichiers_a_supprimer)
-  gc()
+bv_selection <- bv[i, ] 
+nom_bv <- bv_selection$CODE 
+print(paste("--- Traitement du BV :", nom_bv, "(", i, "/", nrow(bv), ") ---"))
+# Création du Buffer 
+bv_buffer <- st_buffer(bv_selection, dist = 2000)
+
+# ETANGS
+etangs_final <- etangs %>%
+  st_make_valid() %>%
+  st_intersection(bv_buffer) %>%
+  mutate(
+    CODE_ETANG = 0,                 
+    NOM        = stringr::str_extract(nom_p_eau, "(?<=\": \").*(?=\")"),
+    SURFACE_eau = superficie,
+    Exutoire_1 = NA_character_,
+    Exutoire_2 = NA_character_
+    
+  ) %>% 
+  select(nature, insee_com, NOM, SURFACE_eau, CODE_ETANG, Exutoire_1,Exutoire_2)
+
+
+# ROUTES
+routes_final <- routes %>%
+  st_intersection(bv_buffer) %>%
+  select(TYPE_ROUTE = 1) 
+
+#os
+os_final <- OS %>%
+  st_intersection(bv_buffer) %>%
+  select(Classe) 
+
+
+
+#LIDAR (Découpe raster)
+# crop coupe le rectangle, mask coupe selon la forme exacte du buffer
+lidar_final <- crop(lidar, bv_buffer)
+lidar_5m <- aggregate(lidar_final, fact = 10, fun = "mean")
+
+print(paste("Lancement de l'hydrologie GRASS pour le BV :", nom_bv))
+
+mnt_temp <- "GPKG_Sortie/temp_mnt.tif"
+writeRaster(lidar_5m, mnt_temp, overwrite = TRUE)
+
+#Préparation des chemins de sortie pour les 4 fichiers 
+acc_tif  <- paste0("GPKG_Sortie/temp_acc_", nom_bv, ".tif")
+dir_tif  <- paste0("GPKG_Sortie/temp_dir_", nom_bv, ".tif")
+bas_tif  <- paste0("GPKG_Sortie/temp_bas_", nom_bv, ".tif")
+demi_tif <- paste0("GPKG_Sortie/temp_demi_", nom_bv, ".tif")
+
+seuil_pixels <- (as.numeric(bv_selection$SURFACE) * 10000) / 25
+
+
+
+qgis_run_algorithm(
+  "grass:r.watershed",
+  elevation    = mnt_temp,
+  threshold    = seuil_pixels,
+  accumulation = acc_tif,
+  drainage     = dir_tif,
+  basin        = bas_tif,
+  half_basin   = demi_tif,
+  "-s"         = TRUE,
+  .quiet       = TRUE 
+)
+
+
+bas_poly <- as.polygons(rast(bas_tif)) %>% 
+  st_as_sf() %>% 
+  st_make_valid()
+
+demi_poly <- as.polygons(rast(demi_tif)) %>% 
+  st_as_sf() %>% 
+  st_make_valid()
+
+
+
+# Définir le nom du fichier GeoPackage de sortie
+nom_fichier_gpkg <- paste0("GPKG_Sortie/BV_", nom_bv, "_Complet.gpkg")
+if (file.exists(nom_fichier_gpkg)) file.remove(nom_fichier_gpkg)
+
+
+
+writeRaster(
+  lidar_final, 
+  filename = nom_fichier_gpkg, 
+  names = "MNT_50cm",
+  filetype = "GPKG",
+  overwrite = TRUE
+)
+
+writeRaster(
+  lidar_5m, 
+  filename = nom_fichier_gpkg, 
+  names = "MNT_5m",
+  filetype = "GPKG",
+  gdal = c("APPEND_SUBDATASET=YES", "RASTER_TABLE=MNT_5m")
+)
+
+writeRaster(rast(acc_tif),  filename = nom_fichier_gpkg, names = "Accumulation",       filetype = "GPKG",datatype = "FLT4S", gdal = c("APPEND_SUBDATASET=YES", "RASTER_TABLE=Accumulation"))
+writeRaster(rast(dir_tif),  filename = nom_fichier_gpkg, names = "Direction_Drainage", filetype = "GPKG", gdal = c("APPEND_SUBDATASET=YES", "RASTER_TABLE=Direction_Drainage"))
+
+# auvegarde des VECTEURS
+st_write(etangs_final, nom_fichier_gpkg, layer = "Etangs", append = TRUE, quiet = TRUE, layer_options = "GEOMETRY_NAME=geom") 
+st_write(routes_final, nom_fichier_gpkg, layer = "Routes", append = TRUE, quiet = TRUE, layer_options = "GEOMETRY_NAME=geom")
+st_write(os_final, nom_fichier_gpkg, layer = "OS", append = TRUE, quiet = TRUE, layer_options = "GEOMETRY_NAME=geom") 
+st_write(bv_selection, nom_fichier_gpkg, layer = "bv_life", append = TRUE, quiet = TRUE, layer_options = "GEOMETRY_NAME=geom") 
+st_write(bas_poly,  nom_fichier_gpkg, layer = "Bassins_Versant", append = TRUE, quiet = TRUE, layer_options = "GEOMETRY_NAME=geom")
+st_write(demi_poly, nom_fichier_gpkg, layer = "Demi_Bassins", append = TRUE, quiet = TRUE, layer_options = "GEOMETRY_NAME=geom")
+
+
+
+
+#style
+con_dest <- dbConnect(SQLite(), nom_fichier_gpkg)
+
+#STYLE ÉTANGS
+style_etangs <- style_data
+style_etangs$f_table_name <- "Etangs"
+style_etangs$f_geometry_column <- "geom"
+style_etangs$useasdefault <- 1   
+
+#STYLE OS
+style_os <- style_os_modele
+style_os$f_table_name <- "OS"
+style_os$f_geometry_column <- "geom"
+style_os$useasdefault <- 1       
+
+#STYLE ACCUMULATION
+style_acc <- style_acc_modele
+style_acc$f_table_name <- "Accumulation"
+style_acc$f_geometry_column <- ""  
+style_acc$styleqml <- qml_texte
+style_acc$stylename <- "Style Accumulation"
+style_acc$useasdefault <- 1
+
+# On assemble tout
+styles_complet <- rbind(style_etangs, style_os, style_acc)
+if("id" %in% names(styles_complet)) styles_complet <- styles_complet %>% select(-id)
+
+# Écriture dans SQLite
+dbWriteTable(con_dest, "layer_styles", styles_complet, overwrite = TRUE, row.names = FALSE)
+dbExecute(con_dest, "CREATE INDEX IF NOT EXISTS msg_layer_styles_idx ON layer_styles (f_table_name, f_geometry_column);")
+dbDisconnect(con_dest)
+
+# Nettoyage
+fichiers_a_supprimer <- list.files(path = "GPKG_Sortie", pattern = "^temp_", full.names = TRUE)
+file.remove(fichiers_a_supprimer)
+gc()
 }
 
 
@@ -203,16 +202,15 @@ for (i in which(bv$CODE %in% c(19,1))) {
 
 print("Démarrage de l'injection des tableaux et des formulaires...")
 
-# 1. On charge les données des tableaux purs
+# On charge les données des tableaux 
 table_assec <- st_read("SIG/assecevolage/Assec.gpkg", quiet = TRUE) %>% 
-  st_drop_geometry() %>% 
-  as.data.frame()
+st_drop_geometry() %>% 
+as.data.frame()
 
 table_peche <- st_read("SIG/peche_Vidange/peche_Vidange.gpkg", quiet = TRUE) %>% 
-  st_drop_geometry() %>% 
-  as.data.frame()
+st_drop_geometry() %>% 
+as.data.frame()
 
-# 2. On "POMPE" LES STYLES (Formulaires QGIS) depuis les fichiers sources
 con_src_assec <- dbConnect(SQLite(), "SIG/assecevolage/Assec.gpkg")
 style_assec <- dbGetQuery(con_src_assec, "SELECT * FROM layer_styles LIMIT 1")
 dbDisconnect(con_src_assec)
@@ -235,35 +233,29 @@ styles_tableaux <- rbind(style_assec, style_peche)
 if("id" %in% names(styles_tableaux)) styles_tableaux <- styles_tableaux %>% select(-id)
 
 
-# 3. On liste tous les GeoPackages de la Boucle 1
+# On liste tous les GeoPackages de la Boucle 1
 liste_gpkg_finis <- list.files(path = "GPKG_Sortie", pattern = "^BV_.*_Complet\\.gpkg$", full.names = TRUE)
 
-# 4. On boucle sur chaque fichier
+# On boucle sur chaque fichier
 for (fichier_cible in liste_gpkg_finis) {
-  
-  nom_court <- basename(fichier_cible)
-  print(paste("-> Ajout des tableaux et formulaires dans :", nom_court))
-  
-  # A) Écriture des données (génère la clé primaire "fid" pour rendre éditable)
-  st_write(table_assec, dsn = fichier_cible, layer = "Tableau_Assec", 
-           driver = "GPKG", append = TRUE, quiet = TRUE)
-  
-  st_write(table_peche, dsn = fichier_cible, layer = "Tableau_peche_Vidange", 
-           driver = "GPKG", append = TRUE, quiet = TRUE)
-  
-  # B) Injection des formulaires dans la base SQLite
-  con_cible <- dbConnect(SQLite(), fichier_cible)
-  # dbAppendTable rajoute nos 2 lignes à la suite des styles Étangs et OS créés en Boucle 1 !
-  dbAppendTable(con_cible, "layer_styles", styles_tableaux)
-  dbDisconnect(con_cible)
+
+nom_court <- basename(fichier_cible)
+print(paste("-> Ajout des tableaux et formulaires dans :", nom_court))
+
+st_write(table_assec, dsn = fichier_cible, layer = "Tableau_Assec", 
+         driver = "GPKG", append = TRUE, quiet = TRUE)
+
+st_write(table_peche, dsn = fichier_cible, layer = "Tableau_peche_Vidange", 
+         driver = "GPKG", append = TRUE, quiet = TRUE)
+
+# Injection des formulaires dans la base SQLite
+con_cible <- dbConnect(SQLite(), fichier_cible)
+# dbAppendTable rajoute nos 2 lignes à la suite des styles
+dbAppendTable(con_cible, "layer_styles", styles_tableaux)
+dbDisconnect(con_cible)
 }
 
-print("✅ Opération terminée avec succès ! Tableaux éditables AVEC listes déroulantes et calendriers.")
-
-
-
-
-
+print("pération terminée avec succès!")
 
 
 
